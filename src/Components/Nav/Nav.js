149c4cb85f00646b812a151a0366fd863a8adb9e
jenny87879 <jenny87879@gmail.com>
2021-02-16 10:41:26 +0900

    Add: Nav Components 초기세팅 완료.

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './Nav.scss';
 
 class Nav extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="Nav">
+        <nav></nav>
+      </div>
+    );
   }
 }
 
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,8 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Nav from '../../Components/Nav/Nav';
+import './Main.scss';
 
 class Main extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="Main">
+        <Nav />
+      </div>
+    );
   }
 }
 
