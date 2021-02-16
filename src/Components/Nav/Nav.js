0cf8d3fb8512c87741fa63dfac1a5e8e3bba48d1
jenny87879 <jenny87879@gmail.com>
2021-02-16 21:28:30 +0900

    Add: nav 바 레이아웃 완료

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 class Nav extends Component {
@@ -10,8 +11,54 @@ class Nav extends Component {
         <nav className={scrollValue ? 'nav-white' : 'nav-transparent'}>
           <div className="nav-container">
             <ul className="nav-left-container">
-              <li>SHOP</li>
-              <li>REVIEW</li>
+              <li className="nav-item">
+                <span>SHOP</span>
+                <section className="dropdown-menu">
+                  <div className="dropdown-link-column">
+                    <h4>VITAMINS</h4>
+                    <ul>
+                      <li className="product-items">
+                        <Link to="/productList">Letter Vitamins</Link>
+                      </li>
+                      <li>
+                        <Link to="/productList">Minerals</Link>
+                      </li>
+                      <li>
+                        <Link to="/productList">Herbs</Link>
+                      </li>
+                      <li>
+                        <Link to="/productList">Probiotics</Link>
+                      </li>
+                      <li>
+                        <Link to="/productList">Specialty</Link>
+                      </li>
+                      <li>
+                        <Link to="/productList">See all</Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="dropdown-link-column">
+                    <h4>POWDERS</h4>
+                    <ul>
+                      <li>
+                        <Link to="/productList">Collagen</Link>
+                      </li>
+                      <li>
+                        <Link to="/productList">Protein</Link>
+                      </li>
+                      <li>
+                        <Link to="/productList">Boosts</Link>
+                      </li>
+                      <li>
+                        <Link to="/productList">See All</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </section>
+              </li>
+              <li>
+                <span>REVIEW</span>
+              </li>
             </ul>
             <div className="logo">core/of</div>
             <ul className="nav-right-container">
