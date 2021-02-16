0d5299a21c1aab4ac18922ecc201b1656e23b6de
jieunlee <jenna8447@naver.com>
2021-02-16 18:42:10 +0900

    Add: 로그인 페이지 레이아웃 완료

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,78 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './Login.scss';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color: 'black',
+      id: '',
+      pw: '',
+    };
+  }
+
+  handleAllInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  // console.log(e.target.name, e.target.value)
+
+  isLogin=()=> {
+    (this.state.id.includes("@") && this.state.pw.length >=5)
+    ?this.setState({})
+    :this.setState({})
+  }
+  
+  // gotoMain = () => {
+  //   fetch(   , {
+  //     method:"POST",
+  //     body: JSON.stringify({
+  //       email: this.state.id,
+  //       password: this.state.pw,
+  //     })
+  //   })
+  //   .then((response) => response.json())
+  //   .then((result) => {
+  //      if (result.message === "SUCCESS") {
+  //        localStorage.setItem('token', result.token);
+  //        this.props.history.push("/main");
+  //        alert("로그인 성공");
+  //      }else { alert("로그인 실패"); console.log(result.message);}
+  //    }
+  //    )
+  //  }  
+ 
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+          <header>
+            <div className='existing_customer'><span>LOGIN AS EXISTING CUSTOMER</span></div>
+          </header>
+          <div className="login_form">
+            <div className='id_form'>
+              <label for="user_email">EMAIL</label>
+              <br />
+              <input name="id" className="id_box" type="text" />
+            </div>
+            <div className='pw_form'>
+              <label for="user_pw">PASSWORD</label>
+              <br />
+              <input name="pw" className="pw_box" type="password" />
+            </div>
+            <div className='check_box'>
+              <input type="checkbox" id="remember_me" name="remember_me"/> 
+                <label for="remember_me">REMEMBER ME</label>
+            </div>
+            <div>
+              <button className="login_btn"><span>Login</span></button>
+            </div>
+            <div className= 'forget_pw'>
+            <a href="https://takecareof.com"><span>Forget Password?</span></a>
+            </div>
+      </div>
+      </div>
+    );
   }
 }
 
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,8 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class SignUp extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="sign_up">
+        <header>
+          <div>Sign Up</div>
+        </header>
+        <form>
+          <div className="name_form">
+            <label for="user_name">Name</label>
+            <br />
+            <input name="id" className="name_box" type="text" placeholder='enter your name'/>
+          </div>
+          <div className="phone_number">
+            <label for="phone_number">Phone number</label>
+            <br />
+            <input name="number" className="number_box" type="text" placeholder='enter your phone number'/> 
+            <button className=''>Send authentication</button>
+          </div>
+          <div className="confirm_number">
+            <input name="confirm" className="confirm_box" type="text" placeholder='enter authentication number' />
+            <button className=''>Confirm</button>
+          </div>
+
+        </form>
+      </div>
+    );
   }
 }
 
