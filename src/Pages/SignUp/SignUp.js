0319d1828f4e81529ee4015a3164c793c42a2464
jieunlee <jenna8447@naver.com>
2021-02-16 18:48:34 +0900

    Add: 로그인 레이아웃 수정 완료

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,30 +1,11 @@
+import { clearConfigCache } from "prettier";
 import React, { Component } from 'react';
 
 class SignUp extends Component {
   render() {
     return (
-      <div className="sign_up">
-        <header>
-          <div>Sign Up</div>
-        </header>
-        <form>
-          <div className="name_form">
-            <label for="user_name">Name</label>
-            <br />
-            <input name="id" className="name_box" type="text" placeholder='enter your name'/>
-          </div>
-          <div className="phone_number">
-            <label for="phone_number">Phone number</label>
-            <br />
-            <input name="number" className="number_box" type="text" placeholder='enter your phone number'/> 
-            <button className=''>Send authentication</button>
-          </div>
-          <div className="confirm_number">
-            <input name="confirm" className="confirm_box" type="text" placeholder='enter authentication number' />
-            <button className=''>Confirm</button>
-          </div>
-
-        </form>
+      <div>
+        
       </div>
     );
   }
