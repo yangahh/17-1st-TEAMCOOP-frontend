8526b9250cfa645b5d7681aa1037f8fcfb531aad
jenny87879 <jenny87879@gmail.com>
2021-02-16 19:40:56 +0900

    Add: nav 바 레이아웃 완료

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,9 +3,37 @@ import './Nav.scss';
 
 class Nav extends Component {
   render() {
+    const { scrollValue } = this.props;
+
     return (
       <div className="Nav">
-        <nav></nav>
+        <nav className={scrollValue ? 'nav-white' : 'nav-transparent'}>
+          <div className="nav-container">
+            <ul className="nav-left-container">
+              <li>SHOP</li>
+              <li>REVIEW</li>
+            </ul>
+            <div className="logo">core/of</div>
+            <ul className="nav-right-container">
+              <li>SIGN IN</li>
+              <li>
+                <img
+                  alt="user_icon"
+                  src="https://www.flaticon.com/svg/vstatic/svg/633/633779.svg?token=exp=1613461834~hmac=347842e2efa7c2db1f43caff23b7df96"
+                />
+              </li>
+              <li>
+                <img
+                  alt="cart_icon"
+                  src="https://www.flaticon.com/svg/vstatic/svg/685/685370.svg?token=exp=1613466029~hmac=c80aa4d64a80d25a482b121b896f549f"
+                />
+              </li>
+              <li>
+                <button>Take the quiz</button>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -3,10 +3,37 @@ import Nav from '../../Components/Nav/Nav';
 import './Main.scss';
 
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scrollValue: 0,
+    };
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.getScrolloValue);
+  };
+
+  componentWillUnMount = () => {
+    window.removeEventListener('scroll', this.getScrolloValue);
+  };
+
+  getScrolloValue = event => {
+    const scrollTop = ('scroll', event.srcElement.scrollingElement.scrollTop);
+    this.setState({
+      scrollValue: scrollTop,
+    });
+  };
+
   render() {
+    const { scrollValue } = this.state;
+
     return (
-      <div className="Main">
-        <Nav />
+      <div className="Main" onScroll={this.getScrolloValue}>
+        <Nav scrollValue={scrollValue} />
+        <div className="layout-content">
+          <div className="main-content"></div>
+        </div>
       </div>
     );
   }
