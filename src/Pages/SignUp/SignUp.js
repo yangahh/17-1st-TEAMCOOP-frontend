31ff40640ee63311d6304d41e2dbf386ffd96247
jieunlee <jenna8447@naver.com>
2021-02-17 16:22:13 +0900

    Add: Sign Up page layout

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name_box: '',
+      number: '',
+      confirm: '',
+      id: '',
+      pw: '',
+    };
+  }
+
+  handleAllInput = e => {
+    this.setState({
+      [e.target.name]: 'e.target.value',
+    });
+  };
+
+  gotoMain = event => {
+    fetch('', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: this.state.name_box,
+        number: this.state.number,
+        confirm: this.state.confirm,
+        email: this.state.id,
+        password: this.state.pw,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.message === '') {
+          this.props.history.push('/login');
+          alert('회원가입 완료');
+        } else {
+          alert('실패');
+        }
+      });
+  };
+
   render() {
+    console.log(this.state.name_box);
     return (
       <div className="sign_up">
         <header>
-          <p>Sign Up</p>
+          <p onClick={this.gotoMain}>Sign Up</p>
         </header>
         <div name="first_content">
           <div className="name_form">
             <label for="user_name">Name</label>
             <br />
             <input
+              onChange={this.handleAllInput}
               name="name_box"
               className="name_box"
               type="text"
@@ -22,6 +63,7 @@ class SignUp extends Component {
             <label for="phone_number">Phone number</label>
             <br />
             <input
+              onChange={this.handleAllInput}
               name="number"
               className="number_box"
               type="text"
@@ -31,6 +73,7 @@ class SignUp extends Component {
           </div>
           <div className="confirm_number">
             <input
+              onChange={this.handleAllInput}
               name="confirm"
               className="confirm_box"
               type="text"
@@ -45,6 +88,7 @@ class SignUp extends Component {
             <label for="user_id">ID(EMAIL)</label>
             <br />
             <input
+              onChange={this.handleAllInput}
               name="id"
               className="id_box"
               type="text"
@@ -55,6 +99,7 @@ class SignUp extends Component {
             <label for="user_pw">PASSWORD</label>
             <br />
             <input
+              onChange={this.handleAllInput}
               name="pw"
               className="pw_box"
               type="text"
@@ -65,20 +110,46 @@ class SignUp extends Component {
             <label for="pw_again">CONFIRM PASSWORD</label>
             <br />
             <input
-              name="pw"
+              onChange={this.handleAllInput}
+              name="pw_again"
               className="pw_box"
               type="text"
               placeholder="enter your pw"
             />
           </div>
+          <div className="agree_all_terms">
+            <input name="agree_all" type="checkbox" className="agree_all" />
+            <label for="agree_all">AGREE ALL TERMS</label>
+          </div>
         </div>
         <div className="third_content">
-        <div class="termsview">
-          <label for="label-checkbox">
-            <input name="termsService" type="checkbox" className="input-checkbox inp-terms" >
-              <span>이용 약관 동의</span> 
-            </label>
-            </div>
+          <div className="termsview1">
+            <input
+              name="termsService"
+              type="checkbox"
+              className="input-checkbox1"
+            />
+            <label>Agree terms view</label>
+            <a href="https://takecareof.com">View Details</a>
+          </div>
+          <div className="termsview2">
+            <input
+              name="termsService"
+              type="checkbox"
+              className="input-checkbox2"
+            />
+            <label>Agree Privacy Policy</label>
+            <a href="https://takecareof.com">View Details</a>
+          </div>
+          <div className="termsview3">
+            <input
+              name="termsService"
+              type="checkbox"
+              className="input-checkbox3"
+            />
+            <label>Agreeing To Recieve Marketing (optional) </label>
+            <a href="https://takecareof.com">View Details</a>
+          </div>
         </div>
       </div>
     );
