c6514cf8d1a77ba3021d64486a97830f8a74fdb1
jenny87879 <jenny87879@gmail.com>
2021-02-17 15:24:01 +0900

    Add: main 페이지 백그라운드 이미지 지정

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,8 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './Main.scss';
 
 class Main extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="Main">
+        <div className="layout-content">
+          <div className="main-view-content">
+            <div className="main-view-text">
+              <div className="main-view-header">
+                You know your body, we know the science. Let’s work together.
+              </div>
+            </div>
+          </div>
+          <div className="main-point-content"></div>
+        </div>
+      </div>
+    );
   }
 }
 
