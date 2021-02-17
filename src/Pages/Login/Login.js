023cf3bba67cbf27b5172807f288c0f37063b39b
jieunlee <jenna8447@naver.com>
2021-02-17 10:51:47 +0900

    ADD: Login 통신 기능 구현중

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -18,30 +18,28 @@ class Login extends Component {
   };
   // console.log(e.target.name, e.target.value)
 
-  isLogin=()=> {
-    (this.state.id.includes("@") && this.state.pw.length >=5)
-    ?this.setState({})
-    :this.setState({})
-  }
+
   
-  // gotoMain = () => {
-  //   fetch(   , {
-  //     method:"POST",
-  //     body: JSON.stringify({
-  //       email: this.state.id,
-  //       password: this.state.pw,
-  //     })
-  //   })
-  //   .then((response) => response.json())
-  //   .then((result) => {
-  //      if (result.message === "SUCCESS") {
-  //        localStorage.setItem('token', result.token);
-  //        this.props.history.push("/main");
-  //        alert("로그인 성공");
-  //      }else { alert("로그인 실패"); console.log(result.message);}
-  //    }
-  //    )
-  //  }  
+  gotoMain = () => {
+    if(this.state.id.includes("@") && this.state.pw.length >=8){
+    fetch("" , {
+      method:"POST", 
+      body: JSON.stringify({
+        email: this.state.id,
+        password: this.state.pw,
+      })
+    })
+    .then((response) => response.json())
+    .then((result) => {
+       if (result.message === "SUCCESS") {
+         localStorage.setItem('token', result.token);
+         this.props.history.push("/");
+         alert("로그인 성공");
+       }else { alert("로그인 실패"); console.log(result.message);}
+     }
+     )
+   }
+  };
  
   render() {
     return (
@@ -53,19 +51,19 @@ class Login extends Component {
             <div className='id_form'>
               <label for="user_email">EMAIL</label>
               <br />
-              <input name="id" className="id_box" type="text" />
+              <input onChange={this.handleAllInput} name="id" className="id_box" type="text" />
             </div>
             <div className='pw_form'>
               <label for="user_pw">PASSWORD</label>
               <br />
-              <input name="pw" className="pw_box" type="password" />
+              <input onChange={this.handleAllInput} name="pw" className="pw_box" type="password" />
             </div>
             <div className='check_box'>
               <input type="checkbox" id="remember_me" name="remember_me"/> 
                 <label for="remember_me">REMEMBER ME</label>
             </div>
             <div>
-              <button className="login_btn"><span>Login</span></button>
+              <button onClick = {this.gotoMain} className="login_btn"><span>Login</span></button>
             </div>
             <div className= 'forget_pw'>
             <a href="https://takecareof.com"><span>Forget Password?</span></a>
