b71238e2a24d6b956c63cd08857f3772d9938fd6
jenny87879 <jenny87879@gmail.com>
2021-02-17 16:23:12 +0900

    Add: Main content 레이아웃 완료

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.scss';
 
 class Main extends Component {
@@ -8,9 +9,19 @@ class Main extends Component {
         <div className="layout-content">
           <div className="main-view-content">
             <div className="main-view-text">
-              <div className="main-view-header">
+              <h2 className="main-view-header">
                 You know your body, we know the science. Let’s work together.
-              </div>
+              </h2>
+              <p className="main-view-subheader">
+                Find the right vitamins, protein, and now collagen too,
+                personalized just for you.
+              </p>
+              <Link to="/quiz">
+                <button className="quiz-button">Take the quiz</button>
+              </Link>
+              <Link to="/productlist" className="product-link">
+                Browse all products
+              </Link>
             </div>
           </div>
           <div className="main-point-content"></div>
