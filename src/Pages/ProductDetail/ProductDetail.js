3cf3caf12ac7e8a515c39c6ef224d3a4b3e49bf2
jenny87879 <68068885+jenny87879@users.noreply.github.com>
2021-02-19 03:27:19 +0900

    Add: main product container 레이아웃 완료

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -5,7 +5,12 @@ class ProductDetail extends Component {
   render() {
     return (
       <div className="ProductDetail">
-        <nav className="product-nav"></nav>
+        <nav className="product-nav">
+          <div className="nav-container">
+            11
+          </div>
+
+        </nav>
         <section className="main-product-container">
           <div className="product-image-content">
             <img
@@ -14,7 +19,30 @@ class ProductDetail extends Component {
               className="product-image"
             />
           </div>
-          <div className="product-content"></div>
+          <div className="product-content">
+            <ul className="tag-list">
+              <li>SKIN</li>
+              <li>NAILS</li>
+            </ul>
+            <h1 className="display-title">Collagen-matcha</h1>
+            <h2 className="sub-title">The Skin Hero</h2>
+            <ul className="description">
+              <li>Supports hydration and elasticity in skin*</li>
+              <li>Helps strengthen nails*</li>
+              <li>Grass-fed bovine collagen</li>
+              <li>Made with premium Japanese matcha powder</li>
+            </ul>
+            <a href="" className="detail-link">See supplement facts</a>
+            <select name="product-count" className="product-count">
+              <option value="">15 Serving Tub</option>
+              <option value="">Matcha</option>
+            </select>
+            <button className="add-cart-btn">
+              Add
+              <span className="btn-line"/>
+              $32
+            </button>
+          </div>
         </section>
         <section className="quality-badges-container">2</section>
         <section className="overview-container">3</section>
