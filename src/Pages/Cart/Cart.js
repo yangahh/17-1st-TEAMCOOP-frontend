c4cd52b37ba9019ab9fc5edbef0a2366318fdde4
saemsol <yoosaemsol@gmail.com>
2021-02-18 16:14:58 +0900

    Add: Card 컴포넌트 초기세팅 완료

---
+++ src/Pages/Cart/Cart.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Cart.scss';
+
+class Cart extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Cart;
--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Main from "./Pages/Main/Main";
-import Login from "./Pages/Login/Login";
-import SignUp from "./Pages/SignUp/SignUp";
-import ProductList from "./Pages/ProductList/ProductList";
-import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import Main from './Pages/Main/Main';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
+import ProductList from './Pages/ProductList/ProductList';
+import ProductDetail from './Pages/ProductDetail/ProductDetail';
+import Cart from './Pages/Cart/Cart';
 
 class Routes extends React.Component {
   render() {
@@ -17,6 +18,7 @@ class Routes extends React.Component {
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/productlist" component={ProductList} />
           <Route exact path="/productdetail/:id" component={ProductDetail} />
+          <Route exact path="/cart" component={Cart} />
         </Switch>
       </Router>
     );
