4a65a74ce383900dfbba5678c345268b638921cb
jieunlee <jenna8447@naver.com>
2021-02-19 19:43:18 +0900

    Add: 로그인 레이아웃 수정 완료

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,7 +5,6 @@ class Login extends Component {
   constructor() {
     super();
     this.state = {
-      color: 'black',
       id: '',
       pw: '',
     };
@@ -16,59 +15,37 @@ class Login extends Component {
       [e.target.name]: e.target.value,
     });
   };
-  // console.log(e.target.name, e.target.value)
-
-
-  
-  gotoMain = () => {
-    if(this.state.id.includes("@") && this.state.pw.length >=8){
-    fetch("" , {
-      method:"POST", 
-      body: JSON.stringify({
-        email: this.state.id,
-        password: this.state.pw,
-      })
-    })
-    .then((response) => response.json())
-    .then((result) => {
-       if (result.message === "SUCCESS") {
-         localStorage.setItem('token', result.token);
-         this.props.history.push("/");
-         alert("로그인 성공");
-       }else { alert("로그인 실패"); console.log(result.message);}
-     }
-     )
-   }
-  };
  
   render() {
     return (
-      <div className="container">
-          <header>
-            <div className='existing_customer'><span>LOGIN AS EXISTING CUSTOMER</span></div>
-          </header>
-          <div className="login_form">
-            <div className='id_form'>
-              <label for="user_email">EMAIL</label>
-              <br />
-              <input onChange={this.handleAllInput} name="id" className="id_box" type="text" />
-            </div>
-            <div className='pw_form'>
-              <label for="user_pw">PASSWORD</label>
-              <br />
-              <input onChange={this.handleAllInput} name="pw" className="pw_box" type="password" />
-            </div>
-            <div className='check_box'>
+      <div className="Login">
+        <header>
+          <div className='existing_customer'>
+            <span>LOGIN AS EXISTING CUSTOMER</span>
+          </div>
+        </header>
+        <div className="login_form">
+          <div className='id_form'>
+            <label>EMAIL
+              <input type="text" onChange={this.handleAllInput} name="id" className="id_box" /> 
+            </label>
+          </div>
+          <div className='pw_form'>
+            <label>PASSWORD
+              <input type="password" onChange={this.handleAllInput} name="pw" className="pw_box" /> 
+            </label>
+          </div>
+          <div className='check_box'>
+            <label for="remember_me">
               <input type="checkbox" id="remember_me" name="remember_me"/> 
-                <label for="remember_me">REMEMBER ME</label>
-            </div>
-            <div>
-              <button onClick = {this.gotoMain} className="login_btn"><span>Login</span></button>
-            </div>
-            <div className= 'forget_pw'>
-            <a href="https://takecareof.com"><span>Forget Password?</span></a>
-            </div>
-      </div>
+              REMEMBER ME
+            </label>
+          </div>
+          <button onClick = {this.gotoMain} className="login_btn">Login</button>
+          <div className= 'forget_pw'>
+            <a href="https://takecareof.com">Forget Password?</a>
+          </div>
+        </div>
       </div>
     );
   }
