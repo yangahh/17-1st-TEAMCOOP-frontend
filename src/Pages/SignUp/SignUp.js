7e60513ec9b8361f3cb0a356654835de9b72f96c
jieunlee <jenna8447@naver.com>
2021-02-19 14:36:53 +0900

    Add: 회원가입 레이아웃 완료

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -45,120 +45,126 @@ class SignUp extends Component {
     console.log(this.state.name_box);
     return (
       <div className="sign_up">
-        <div className= "signup_form">
         <header>
-          <p>Sign Up</p>
+          <span>Sign Up</span>
         </header>
-        <div name="first_content">
-          <div className="name_form">
-            <label for="user_name">Name</label>
-            <br />
-            <input
-              onChange={this.handleAllInput}
-              name="name_box"
-              className="name_box"
-              type="text"
-              placeholder="enter your name"
-            />
+        <div className="signup_form">
+          <div className="first_content">
+            <div className="name_form">
+              <label>Name</label>
+              <input
+                onChange={this.handleAllInput}
+                name="name_box"
+                className="name_box"
+                type="text"
+                placeholder="Enter your name"
+              />
+            </div>
+            <div className="phone_form">
+              <label>Phone number</label>
+              <div className="with_button">
+                <input
+                  onChange={this.handleAllInput}
+                  name="number"
+                  className="number_box"
+                  type="text"
+                  placeholder="Enter your phone number"
+                />
+                <button className="send">Send</button>
+              </div>
+            </div>
+            <div className="confirm_form">
+              <div className="with_button">
+                <input
+                  disabled
+                  onChange={this.handleAllInput}
+                  name="confirm"
+                  className="confirm_box"
+                  type="text"
+                  placeholder="Enter authentication number"
+                />
+                <button className="confirm">Confirm</button>
+              </div>
+            </div>
           </div>
-          <div className="phone_number">
-            <label for="phone_number">Phone number</label>
-            <br />
-            <input
-              onChange={this.handleAllInput}
-              name="number"
-              className="number_box"
-              type="text"
-              placeholder="enter your phone number"
-            />
-            <button className="send">Send authentication</button>
-          </div>
-          <div className="confirm_number">
-            <input
-              onChange={this.handleAllInput}
-              name="confirm"
-              className="confirm_box"
-              type="text"
-              placeholder="enter authentication number"
-            />
-            <button className="confirm">Confirm</button>
-          </div>
-        </div>
-
-        <div name="second_content">
-          <div className="user_id">
-            <label for="user_id">ID(EMAIL)</label>
-            <br />
-            <input
-              onChange={this.handleAllInput}
-              name="id"
-              className="id_box"
-              type="text"
-              placeholder="enter your id"
-            />
-          </div>
-          <div className="user_pw">
-            <label for="user_pw">PASSWORD</label>
-            <br />
-            <input
-              onChange={this.handleAllInput}
-              name="pw"
-              className="pw_box"
-              type="text"
-              placeholder="enter your pw"
-            />
-          </div>
-          <div className="user_pw_again">
-            <label for="pw_again">CONFIRM PASSWORD</label>
-            <br />
-            <input
-              onChange={this.handleAllInput}
-              name="pw_again"
-              className="pw_box"
-              type="text"
-              placeholder="enter your pw"
-            />
-          </div>
-          <div className="agree_all_terms">
-            <input name="agree_all" type="checkbox" className="agree_all" />
-            <label for="agree_all">AGREE ALL TERMS</label>
+          <div className="second_content">
+            <div className="id_form">
+              <label for="user_id">ID (EMAIL)</label>
+              <br />
+              <input
+                onChange={this.handleAllInput}
+                name="id"
+                className="id_box"
+                type="text"
+                placeholder="Enter your ID"
+              />
+            </div>
+            <div className="pw_form">
+              <label for="user_pw">PASSWORD</label>
+              <br />
+              <input
+                onChange={this.handleAllInput}
+                name="pw"
+                className="pw_box"
+                type="text"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div className="pw_again_form">
+              <label for="pw_again">CONFIRM PASSWORD</label>
+              <br />
+              <input
+                onChange={this.handleAllInput}
+                name="pw_again"
+                className="pw_box"
+                type="text"
+                placeholder="enter your password"
+              />
+            </div>
           </div>
         </div>
         <div className="third_content">
-          <div className="termsview1">
-            <input
-              name="termsService"
-              type="checkbox"
-              className="input-checkbox1"
-            />
-            <label>Agree terms view</label>
-            <a href="https://takecareof.com">View Details</a>
+          <div className="agree_all_terms">
+            <input id="agree_all" name="agree_all" type="checkbox" className="agree_all" />
+            <label for="agree_all">AGREE ALL TERMS</label>
           </div>
-          <div className="termsview2">
-            <input
-              name="termsService"
-              type="checkbox"
-              className="input-checkbox2"
-            />
-            <label>Agree Privacy Policy</label>
-            <a href="https://takecareof.com">View Details</a>
-          </div>
-          <div className="termsview3">
-            <input
-              name="termsService"
-              type="checkbox"
-              className="input-checkbox3"
-            />
-            <label>Agreeing To Recieve Marketing (optional) </label>
-            <a href="https://takecareof.com">View Details</a>
+          <div className="termsview">
+            <div className="termsview1">
+              <input 
+                id ="termsService"
+                name="termsService1"
+                type="checkbox"
+                className="input-checkbox1"
+              />
+              <label for='termsService'>Agree terms view</label>
+              <a href="https://takecareof.com">View Details</a>
+            </div>
+            <div className="termsview2">
+              <input
+                id ="termsService2"
+                name="termsService"
+                type="checkbox"
+                className="input-checkbox2"
+              />
+              <label for='termsService2'>Agree Privacy Policy</label>
+              <a href="https://takecareof.com">View Details</a>
+            </div>
+            <div className="termsview3">
+              <input
+                id ="termsService3"
+                name="termsService"
+                type="checkbox"
+                className="input-checkbox3"
+              />
+              <label for='termsService3'>Agreeing To Recieve Marketing (optional) </label>
+              <a href="https://takecareof.com">View Details</a>
+            </div>
           </div>
         </div>
         <div>
-          <button className="signup_btn">
-            <span>SignUp</span>
-          </button>
+          <button className="signup_btn"><span>Sign Up</span></button>
         </div>
-        </div>
+        <div className='copyright'>&copy; Carewith Inc. All Rights Reserved.</div>
       </div>
     );
   }
