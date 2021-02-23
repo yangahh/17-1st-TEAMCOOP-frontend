4f1529b9e8e664d659f8be607c99851361e46b7c
jieunlee <jenna8447@naver.com>
2021-02-23 21:01:34 +0900

    :Add: 별 평점 기능 구현 완료

--- src/Pages/Review/NewReview.js
+++ src/Pages/Review/NewReview.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
-import Pill from "../../images/pill.jpg";
 import Star from "../../images/star.png";
 import Star2 from "../../images/star2.png";
 
 class NewReview extends Component {
   render() {
+    const{userName,title,content,date,img,star} = this.props;
+     
     return (
       <form className="review_form">
       <div className="left_content">
       <div className="user_name">
-       <p>{this.props.review.userName}</p>
+       <p>{userName}</p>
       </div>
       <div className="review_image">
-      <img src={Pill} />
+      <img src={img} />
       </div>
       </div>
       <div className="right_content">
-      <img className="rating_stars" src={Star} alt="stars"/>
-      <img className="rating_stars" src={Star} alt="stars"/>
-      <img className="rating_stars" src={Star} alt="stars"/>
-      <img className="rating_stars" src={Star2} alt="stars"/>
-      <div className="rating_title">
-        {this.props.review.title}
+          <span>{Array(Number(star) + 1).join('★')}{Array(5-Number(star) + 1).join('☆')}</span>
+        <div className="rating_title">
+          {title}
         </div>
         <div className="review_content">
-       {this.props.review.content}
-      </div>
-      <div className="date">
-       {this.props.review.date}
-      </div>
+          {content}
+        </div>
+        <div className="date">
+          {date}
+        </div>
       </div>
     </form>
     );
--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -1,33 +1,28 @@
 import React, { Component } from 'react';
 import './Review.scss';
-
 import NewReview from './NewReview';
 
 class Review extends Component {
   constructor() {
     super();
-      this.state = ({
-        reviews: [
-          {
-            userName: 'JIEUN',
-            title: 'First off we got sucked into the ...',
-            content:
-              'First off we got sucked into the marketing and prettiness of this company and simple ease of figuring out my "problems" with a quiz. lol Once received, pretty packaging, and so on and so forth. Vitamins, whatever. Be careful those that are celaic. I don\'t recommend. Everything has sugar in it. Both the Whey and protein powder tastes bad. Not as good as those who specialize in it from a natural foods store for example.',
-
-            date: 'JAN 01',
-          },
-          {
-            userName: 'SAEMSOL',
-            title: 'First off we got sucked into the ...',
-            content:
-              'First off we got sucked into the marketing and prettiness of this company and simple ease of figuring out my "problems" with a quiz. lol Once received, pretty packaging, and so on and so forth. Vitamins, whatever. Be careful those that are celaic. I don\'t recommend. Everything has sugar in it. Both the Whey and protein powder tastes bad. Not as good as those who specialize in it from a natural foods store for example.',
-
-            date: 'JAN 03',
-          },
-        ],
-      });
+    this.state = {
+      reviewData: [],
+    };
   }
 
+  componentDidMount() {
+    fetch('/data/reviewData.json', {
+      method: 'GET',
+      //'http://10.58.2.243:8000/order/review'
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          reviewData: res
+          // reviewData: res.data,
+        });
+      });
+  }
   render() {
     return (
       <div className="Review">
@@ -44,8 +39,16 @@ class Review extends Component {
           </div>
         </header>
         {/* 첫번째폼 */}
-        {this.state.reviews.map(apple => (
-          <NewReview review={apple} />
+        {this.state.reviewData.map(review => (
+          <NewReview
+            img={review.img}
+            userName={review.userName}
+            title={review.title}
+            content={review.content}
+            date={review.date}
+            img={review.img}
+            star={review.star}
+          />
         ))}
       </div>
     );
