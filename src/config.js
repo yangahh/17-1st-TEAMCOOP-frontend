c36721b9e0bf721f13735d7a3862447c3cf824d1
saemsol <yoosaemsol@gmail.com>
2021-02-24 14:00:49 +0900

    Modify: 백엔드 통신 주소 변경

--- src/config.js
+++ src/config.js
@@ -7,4 +7,4 @@
 //토큰
 //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoxfQ.KxbzGpPvDDUbXJJfxK89MY-zIJ1-r3SQQjkNdRw3CPY
 // export const SERVER = 'http://10.58.6.180:8000';
-export const SERVER = 'http://192.168.0.10:8000';
+export const SERVER = 'http://10.58.2.178:8000';
