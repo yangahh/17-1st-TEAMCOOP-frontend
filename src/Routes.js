0359e035f55200f99081ed801a4df90ff08f4d66
jenny87879 <jenny87879@gmail.com>
2021-02-25 21:12:16 +0900

    merge 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Main from "./Pages/Main/Main";
-import Login from "./Pages/Login/Login";
-import SignUp from "./Pages/SignUp/SignUp";
-import ProductList from "./Pages/ProductList/ProductList";
-import ProductDetail from "./Pages/ProductDetail/ProductDetail";
-import Nav from "./Components/Nav/Nav"
-import Mypage from "./Pages/MyPage/Mypage"
-import Review from "./Pages/Review/Review"
+import Main from './Pages/Main/Main';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
+import ProductList from './Pages/ProductList/ProductList';
+import ProductDetail from './Pages/ProductDetail/ProductDetail';
+import Nav from './Components/Nav/Nav';
+import Mypage from './Pages/MyPage/Mypage';
+import Review from './Pages/Review/Review';
+import Cart from './Pages/Cart/Cart';
 
 class Routes extends React.Component {
   render() {
